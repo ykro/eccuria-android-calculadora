ae36e6db82cbbe19ca55685f3944424faed99144
ykro <aacs85@gmail.com>
2014-08-03 12:37:10 -0600

    Trabajo en vistas y strings

--- app/src/main/java/com/eccuria/demos/calculadora/MainActivity.java
+++ app/src/main/java/com/eccuria/demos/calculadora/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
@@ -13,7 +16,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
