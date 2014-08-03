b5065d3f8de5c6573815785aa5595a98b2310e0a
ykro <aacs85@gmail.com>
2014-08-03 15:44:50 -0600

    Trabajo en código y strings

--- app/src/main/java/com/eccuria/demos/calculadora/MainActivity.java
+++ app/src/main/java/com/eccuria/demos/calculadora/MainActivity.java
@@ -1,20 +1,34 @@
 package com.eccuria.demos.calculadora;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
-
+    private double tipPercentage = -1;
+    private EditText editTextTotal;
+    public final static String KEY_TOTAL_INCLUDING_TIP = "totalIncludingTip";
+    public final static String TAG = MainActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSubmit = (Button)findViewById(R.id.btnSubmit);
+        editTextTotal = (EditText)findViewById(R.id.total);
+        RadioGroup radioGroupTip = (RadioGroup) findViewById(R.id.radioGroupTip);
+
+        btnSubmit.setOnClickListener(new buttonListener());
+        radioGroupTip.setOnCheckedChangeListener(new radioChangeListener());
     }
 
     @Override
@@ -34,5 +48,47 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    class buttonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG,"El botón ha sido presionado");
+            if (tipPercentage != -1) {
+                double total = 0.0;
+                String txtTotal = editTextTotal.getText().toString().trim();
+
+                if (txtTotal.equals("")) {
+                    Toast.makeText(MainActivity.this, R.string.err_editText_empty, Toast.LENGTH_SHORT).show();
+                } else {
+                    total = Double.parseDouble(txtTotal);
+                    total += total*tipPercentage;
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra(KEY_TOTAL_INCLUDING_TIP, total);
+                    startActivity(intent);
+                }
+            } else {
+                Toast.makeText(MainActivity.this, R.string.err_option_not_selected, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    class radioChangeListener implements RadioGroup.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            Log.i(TAG,"Cambio en el radio group");
+            switch (checkedId) {
+                case R.id.radio10p:
+                    tipPercentage = 0.10;
+                    break;
+                case R.id.radio15p:
+                    tipPercentage = 0.15;
+                    break;
+                case R.id.radio18p:
+                    tipPercentage = 0.18;
+                    break;
+            }
+
+        }
     }
 }
